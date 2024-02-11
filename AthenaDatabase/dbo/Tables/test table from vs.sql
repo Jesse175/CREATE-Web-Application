8177f9c78e0bc2e5b5061cb2b6aeb48c087fdfdc
Jesse Tan <tanleyang7@gmail.com>
2024-02-11 13:12:37 -0600

    connection string

---
+++ AthenaDatabase/dbo/Tables/test table from vs.sql
@@ -0,0 +1,4 @@
+﻿CREATE TABLE [dbo].[test table from vs]
+(
+	[Id] INT NOT NULL PRIMARY KEY
+)
---
+++ AthenaDatabase/dbo/Tables/user.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[user] (
+    [ID]        INT         NOT NULL,
+    [firstName] NCHAR (100) NOT NULL,
+    [lastName]  NCHAR (100) NOT NULL,
+    [email]     NCHAR (100) NOT NULL,
+    [password]  NCHAR (100) NOT NULL,
+    CONSTRAINT [PK_user] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
