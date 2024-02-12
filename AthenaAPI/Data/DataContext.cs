d80732f831d1c21a3b20f269e3a367d025fcc928
Jesse Tan <tanleyang7@gmail.com>
2024-02-11 19:54:21 -0600

    added DataContext, UsersController, changed connection string

---
+++ AthenaAPI/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using AthenaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AthenaAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>().ToTable("user");
+        }
+    }
+}
