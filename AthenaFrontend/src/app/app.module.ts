41af3fb02c0f7b728e283142fbf0a89f25db5777
Lance Balstad <Lance.balstad@ndsu.edu>
2024-02-15 17:58:11 -0600

    Adding outline for login component

--- AthenaFrontend/src/app/app.module.ts
+++ AthenaFrontend/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { StudentsPageComponent } from './components/students-page/students-page.component';
 
