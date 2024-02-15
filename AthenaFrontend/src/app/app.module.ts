1f7debdea71a5cdfd7bcc75a764e7eea1030e6fa
Marisa Rose <marisa.rose@ndsu.edu>
2024-02-15 11:38:50 -0600

    Initial frontend commit

---
+++ AthenaFrontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { StudentsPageComponent } from './components/students-page/students-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    StudentsPageComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ AthenaFrontend/src/app/components/students-page/students-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-students-page',
+  templateUrl: './students-page.component.html',
+  styleUrls: ['./students-page.component.css']
+})
+export class StudentsPageComponent {
+
+}
