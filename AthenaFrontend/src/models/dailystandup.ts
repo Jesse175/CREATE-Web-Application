3859d56c18fccd15b5b40ff2978aaacb09d4294c
JoshuaLee1448 <joshuaplee14214@gmail.com>
2024-02-26 17:09:42 -0600

    Base Frontend Model Classes

---
+++ AthenaFrontend/src/models/dailystandup.ts
@@ -0,0 +1,32 @@
+export class DailyStandup {
+    id: number;
+    dateCreated: Date;
+    description: string;
+
+    constructor(id: number, dateCreated: Date, description: string)
+    {
+        this.id = id;
+        this.dateCreated = dateCreated;
+        this.description = description;
+    }
+
+    get getId(): number{
+        return this.id;
+    }
+    get getDateCreated(): Date{
+        return this.dateCreated;
+    }
+    get getDescription(): string{
+        return this.description;
+    }
+
+    set setId(id: number){
+        this.id = id
+    }
+    set setDateCreated(dateCreated: Date){
+        this.dateCreated = dateCreated;
+    }
+    set setDescription(description: string){
+        this.description = description;
+    }
+}
