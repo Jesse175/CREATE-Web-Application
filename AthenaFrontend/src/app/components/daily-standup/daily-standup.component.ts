6dca96194024803da45442448779231b87051d51
Lance Balstad <Lance.balstad@ndsu.edu>
2024-04-06 22:19:21 -0500

    DS.ts

--- AthenaFrontend/src/app/components/daily-standup/daily-standup.component.ts
+++ AthenaFrontend/src/app/components/daily-standup/daily-standup.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { DailyStandup } from 'src/models/dailystandup';
+import { MatDialog } from '@angular/material/dialog';
+import { Role } from 'src/models/role.model';
+import { Student } from 'src/models/student.model';
+import { StudentService } from 'src/app/services/student.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { DailyStandupService } from '../../services/dailyStandup.service';
 
 @Component({
   selector: 'app-daily-standup',
@@ -6,5 +13,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./daily-standup.component.css']
 })
 export class DailyStandupComponent {
+  public standups: DailyStandup[] = [];
 
+  constructor(public dialog: MatDialog, public dailyStandupService: DailyStandupService, public snackbar: MatSnackBar) {
+    this.getAllDailyStandups();
+  }
+
+  public async getAllDailyStandups(): Promise<void> {
+    this.standups = [];
+    const response = await this.dailyStandupService.GetAllDailyStandups();
+    for (let ds of response) {
+      const standup = new DailyStandup(ds.id, ds.dateCreated, ds.description);
+      this.standups.push(standup);
+    }
+  }
 }
