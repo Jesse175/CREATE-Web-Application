2f4c674fdc618a3230e80d5044805980e90c6464
Lance Balstad <Lance.balstad@ndsu.edu>
2024-04-06 22:20:38 -0500

    DSCard.ts

--- AthenaFrontend/src/app/components/daily-standup/daily-standup-card/daily-standup-card.component.ts
+++ AthenaFrontend/src/app/components/daily-standup/daily-standup-card/daily-standup-card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DailyStandup } from 'src/models/dailystandup';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-daily-standup-card',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./daily-standup-card.component.css']
 })
 export class DailyStandupCardComponent {
+  @Input() standups: DailyStandup[] = [];
+
+  statusText = 'Not Completed'
+
+  getDateFromISOString(): string {
+    const now = new Date();
+    const month = now.getMonth() + 1;
+    const day = now.getDate();
+    const year = now.getFullYear();
+    const currentDate = month + '/' + day + '/' + year;
+    return currentDate;
+  }
+
+  completeDailyStandup(): void {
+    this.statusText = "Completed";
+  }
 
 }
+
