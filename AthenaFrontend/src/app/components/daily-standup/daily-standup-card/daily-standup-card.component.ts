79557ac9106f97ecac3c165fd814c87406fbe092
Lance Balstad <Lance.balstad@ndsu.edu>
2024-04-06 22:18:27 -0500

    Made DS components

---
+++ AthenaFrontend/src/app/components/daily-standup/daily-standup-card/daily-standup-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-standup-card',
+  templateUrl: './daily-standup-card.component.html',
+  styleUrls: ['./daily-standup-card.component.css']
+})
+export class DailyStandupCardComponent {
+
+}
---
+++ AthenaFrontend/src/app/components/daily-standup/daily-standup.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-standup',
+  templateUrl: './daily-standup.component.html',
+  styleUrls: ['./daily-standup.component.css']
+})
+export class DailyStandupComponent {
+
+}
