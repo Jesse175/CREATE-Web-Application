ad51219891239a52e409dc1b7a8adfa71e1da8f5
Lance Balstad <Lance.balstad@ndsu.edu>
2024-04-09 21:31:57 -0500

    Making it so standups only show up on students page

--- AthenaFrontend/src/app/components/daily-standup/daily-standup.component.ts
+++ AthenaFrontend/src/app/components/daily-standup/daily-standup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DailyStandup } from 'src/models/dailystandup';
 import { MatDialog } from '@angular/material/dialog';
 import { Role } from 'src/models/role.model';
@@ -6,6 +6,8 @@ import { Student } from 'src/models/student.model';
 import { StudentService } from 'src/app/services/student.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { DailyStandupService } from '../../services/dailyStandup.service';
+import { AuthToken } from 'src/models/authtoken.model';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-daily-standup',
@@ -14,9 +16,15 @@ import { DailyStandupService } from '../../services/dailyStandup.service';
 })
 export class DailyStandupComponent {
   public standups: DailyStandup[] = [];
+  public role: any;
+  protected auth: any;
 
-  constructor(public dialog: MatDialog, public dailyStandupService: DailyStandupService, public snackbar: MatSnackBar) {
+  constructor(private authService: AuthService, public dailyStandupService: DailyStandupService, public snackbar: MatSnackBar) {
     this.getAllDailyStandups();
+  }
+
+  private async getAuthentication(): Promise<AuthToken> {
+    return await this.authService.getAuthentication();
   }
 
   public async getAllDailyStandups(): Promise<void> {
@@ -26,5 +34,11 @@ export class DailyStandupComponent {
       const standup = new DailyStandup(ds.standupID, ds.userID, ds.dateCreated, ds.description);
       this.standups.push(standup);
     }
+  }
+
+  public async ngOnInit(): Promise<void> {
+      const response = await this.getAuthentication();
+      this.auth = new AuthToken(response);
+      this.role = this.auth.Role;
   }
 }
